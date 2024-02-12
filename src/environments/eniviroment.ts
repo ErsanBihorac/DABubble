35bae4ca21a2ea92c159b54dbbeace28c7917d9c
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-02-12 17:01:18 +0100

    my own firebase

--- src/environments/eniviroment.ts
+++ src/environments/eniviroment.ts
@@ -1,10 +1,10 @@
 export const environment = {
     firebase: {
-        apiKey: "AIzaSyBwQtoQFZ1UWiUgbOSwrfARsckBgdqZjXA",
-        authDomain: "dabubble-5ec88.firebaseapp.com",
-        projectId: "dabubble-5ec88",
-        storageBucket: "dabubble-5ec88.appspot.com",
-        messagingSenderId: "540601189295",
-        appId: "1:540601189295:web:d4cc3d7153a0c9ffa9655a"
+        apiKey: "AIzaSyD9GtnKAF4zF4eMztpbxf9Fe0Mt0LVpBP4",
+        authDomain: "dabubble-portfolio.firebaseapp.com",
+        projectId: "dabubble-portfolio",
+        storageBucket: "dabubble-portfolio.appspot.com",
+        messagingSenderId: "1055988406733",
+        appId: "1:1055988406733:web:8cc494d9e33476b15d8c84"
     }
 };
